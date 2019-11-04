71e78d6f6c5bba9ae8a295d5486733cb15da99a6
jsyme <jsyme222@gmail.com>
2019-11-04 08:18:12 -0700

    Creating Service File

--- django_deploy.py
+++ django_deploy.py
@@ -3,82 +3,179 @@ import os
 import getpass
 
 
+def center(text, width=80, delim="-", end="\n"):  # Center multiple line 'text' with 'delim' padding
+	lines = text.split('\n')
+	for line in lines:
+		print(line.center(width, delim) + end)
+
+
+def prompt_user(prompt, require=False):  # Prompt user and loop if required input
+	ask = input(prompt)
+	if require:
+		while ask == '':
+			ask = input(prompt)
+	return ask
+
+
 class GunicornSock:
-	"""Creates the Gunicorn websocket that the Django application will
+	"""
+	Creates the Gunicorn websocket that the Django application will
 	need to have opened to work with the Nginx server.
 	"""
 
 	def __init__(self):
-		print('\nX_X_X_X_X_X_X_X')
-		print('Gunicorn Service Setup')
-		print('X_X_X_X_X_X_X_X\n')
-
-		self.template = './file_templates/gunicorn/gunicorn.service'
-
-	def prompt_user(self, prompt, require=True):
-		ask = input(prompt)
-		if require:
-			while ask == '':
-				ask = input(prompt)
-		return ask
+		intro = 'Gunicorn Service Setup'
+		print('\n')
+		center(intro, end="\n\n")
 
 	def get_project_info(self):
-		print('\n------------------')
-		print('Information needed to set up')
-		print('Gunicorn service files')
-		print('------------------\n')
+		intro = 'Information needed to set up\nGunicorn service files'
+
+		print('\n')
+		center(intro)
+		print('\n')
 
 		def get_user():
 			print('\n')
 
-			user_prompt = f"USER to run service under-\nIf left blank will use-> {getpass.getuser()}\nUsername: "
-			username = self.prompt_user(user_prompt, require=False)
+			user_prompt = f"USER to run service under-\nDEFAULT: {getpass.getuser()}\n[Username] : "
+			username = prompt_user(user_prompt)
 			if username == '':
 				username = getpass.getuser()
 
-			print(f"User: {username}")
+			center(f"\nUSER\n{username}", width=40, delim=" ", end="")
 			return username
 
 		def get_root_dir():
 			print('\n')
 
-			def check_dir(dir):
-				if os.path.isdir(root_dir):
-					return True
-				else:
-					return False
+			user_prompt = f"DIRECTORY to project root.\nThis should be the same as the \nproject settings 'BASE_DIR'\nDEFAULT: {os.getcwd()}\n[Directory] : "
 
-			user_prompt = "DIRECTORY to project root.\nThis should be the same as the \nproject settings 'BASE_DIR\nDirectory: "
-			root_dir = self.prompt_user(user_prompt)
+			root_dir = prompt_user(user_prompt)
+			if root_dir == '':
+				root_dir = os.getcwd()
 
-			while not check_dir(root_dir):
+			while not os.path.isdir(root_dir):
 				if root_dir:
-					print('\nX_X_ERROR_X_X')
-					print(f'Directory \'{root_dir}\' not valid\n')
+					print('\n')
+					center('ERROR', width=40, delim="x")
+					center(f'Directory \'{root_dir}\' not valid', width=40)
 				root_dir = input(user_prompt)
 
-			print(f"Project root: {root_dir}")
+			center(f"\nBASE DIRECTORY\n{root_dir}", width=40, delim=" ", end="")
 			return root_dir
 
 		def get_project_name():
 			print('\n')
 
-			user_prompt = "PORJECT NAME\nName: "
-			project_name = self.prompt_user(user_prompt)
+			user_prompt = "PORJECT NAME\n[Project] : "
+			project_name = prompt_user(user_prompt, require=True)
 
+			center(f'\nPROJECT\n{project_name}', width=40, delim=" ", end="")
 			return project_name
 
+		def get_group():
+			print('\n')
+
+			user_prompt = "GROUP for socket access\nDEFAULT : 'www-data'\n[Group] : "
+			group = prompt_user(user_prompt, require=False)
+			if group == '':
+				group = 'www-data'
+
+			center(f'\nGROUP\n{group}', width=40, delim=" ", end="")
+			return group
+
+		def get_sock_path(root, name):
+			if not root.endswith('/'):
+				root += '/'
+			root += str(name) + '.sock'
+
+			center(f'Creating sock path at\n{root}', delim="*")
+
+			return root
+
+		def get_env_path(root):
+			print('\n')
+
+			root = root.split('/')
+			root = '/'.join(root[:-1])
+			user_prompt = f"Environment path-\nDEFAULT : {root + '/.env'}\n[Env Path] : "
+			path = prompt_user(user_prompt)
+			if path == '':
+				path = root + '/.env'
+			else:
+				if not path.endswith('/'):
+					path = path + '/'
+				path = path + 'bin/gunicorn'
+
+			def check_env_dir(path):
+				bin_dir = path.split('/')[:-1]
+				bin_dir = '/'.join(bin_dir)
+				checked = map(os.path.exists, [bin_dir, path])
+				checked = list(checked)
+				print(checked)
+				if not all(checked):
+					center(f'ERROR', delim="x")
+					if not checked[0]:
+						center(f'Environment not located at\n{bin_dir}')
+					else:
+						center('Gunicorn not installed\nInstall now? (Y/n)')
+						input('Install Gunicorn?')
+					return exit()
+				else:
+					return checked
+
+			center(f'\nENV PATH\n{path}', width=40, delim=" ", end="")
+			return path
+
+		user = get_user()
+		group = get_group()
+		root_dir = get_root_dir()
+		path_to_env = get_env_path(root_dir)
+		project_name = get_project_name()
+		sock_path = get_sock_path(root_dir, project_name)
+
 		return {
-			'user': get_user(),
-			'project_name': get_project_name(),
-			'root_dir': get_root_dir(),
+			'user': user,
+			'group': group,
+			'path_to_env': path_to_env,
+			'project_name': project_name,
+			'root_dir': root_dir,
+			'sock_path': sock_path,
 		}
 
-	def read_temp(self):
-		with open(self.template, 'rb') as temp:
-			temp = temp.read()
-		return temp
+	def create_service_file(self):  # Create gunicorn.service
+		"""
+		Create gunicorn.service file at /etc/systemd/system/gunicorn.service
+		"""
+
+		def read_temp():
+			template = './file_templates/gunicorn/gunicorn.service'
+			if not os.path.isfile(template):
+					print('\n')
+					center('ERROR', width=40, delim="x")
+					center(f'Template not found\n\'{template}\'', width=40)
+					exit()
+			else:
+				with open(template, 'r') as temp:
+					temp = temp.read()
+				return temp
+
+		info = self.get_project_info()
+		print(info)
+		service_file = read_temp()
+		for key in info.keys():
+			service_file = service_file.replace('{ ' + key + ' }', info[key])
+
+		return service_file
 
 
-g = GunicornSock()
-print(g.get_project_info())
+def main():
+	g = GunicornSock()
+	info = g.get_project_info()
+	for k, v in info:
+		center(f'{k}\n{v}\n\n')
+
+
+if __name__ == '__main__':
+	main()
