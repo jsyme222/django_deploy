18307e40fdd973e7ed51d4e29a3b9fbcf341aad5
jsyme <jsyme222@gmail.com>
2019-11-09 00:13:42 -0700

    File read and write successful

--- main/components.py
+++ main/components.py
@@ -4,140 +4,180 @@ import re
 
 
 def center(text, width=80, delim="-", end="\n"):
-	"""
+    """
 	Text align and decoration for terminal display
+
 	:param text: {str}
 	:param width: {int}
 	:param delim: {str}
 	:param end: {str}
 	:return:
 	"""
-	lines = text.split('\n')
-	for line in lines:
-		print(line.center(width, delim) + end)
+    lines = text.split('\n')
+    for line in lines:
+        print(line.center(width, delim) + end)
 
 
 def check_django_dir(directory):
-	"""
+    """
 	Checks for a 'manage.py' file to verify base directory
 	of a django project
+
 	:param directory: str()
 	:return django_dir: bool()
 	"""
-	scan_files = os.scandir(directory)
-	files = [x.name for x in scan_files]
-	django_dir = True
-	if 'manage.py' not in files:
-		django_dir = False
+    scan_files = os.scandir(directory)
+    files = [x.name for x in scan_files]
+    django_dir = True
+    if 'manage.py' not in files:
+        django_dir = False
 
-	return django_dir
+    return django_dir
 
 
 def default_env_dir(root):
-	"""
+    """
 	Builds a default directory for the virtual
-	environment based off of the 'root' base_dir
-	:param root: str()
+	environment based off of the 'root' base_dir.
+
+	:param root: str(Directory)
 	:return env: str()
 	"""
-	temp = root.split('/')
-	env = '/'.join(temp[:-1])
-	env += '/.env'
 
-	return env
+    temp = root.split('/')
+    env = '/'.join(temp[:-1])
+    env += '/.env/'
+
+    return env
 
 
-class Collector:
+class Collector:  # TODO Get actual file path and file name
 
-	def __init__(self, file):
-		self.errors = {}
+    def __init__(self, file):
+        self.errors = {}
 
-		if not os.path.isfile(file):
-			self.errors['file_error'] = f'Template not found at {file}'
-		else:
-			self.file_path = file
-			with open(file, 'r') as f:
-				self.file = f.read()
+        if not os.path.isfile(file):
+            self.errors['file_error'] = f'Template not found at {file}'
+        else:
+            self.file_path = file
+            with open(file, 'r') as f:
+                self.file = f.read()
 
-	def pull_vars(self):
-		payload = []
-		var_pattern = re.compile('(\\{[\$].*?\\})', re.IGNORECASE | re.DOTALL)
-		variables = var_pattern.findall(self.file)
+    def read_err(self):  # Reads errors
+        for error in self.errors:
+            print(f'{error.upper()}: {self.errors[error]}')
 
-		if len(variables) <= 1:
-			self.errors['File Format'] = 'File not formatted'
+    def pull_vars(self, raw=False):  # TODO docstring
+        payload = []
+        raw_vars = []
+        var_pattern = re.compile('({[$].*?\\})', re.IGNORECASE | re.DOTALL)
+        variables = var_pattern.findall(self.file)
 
-		for var in variables:
-			case = var[3:-2]
-			case = tuple(case.split(', '))
-			prompt, required, default = case[0], case[1], case[2]
-			payload.append((prompt, required, default))
+        if len(variables) < 1:
+            self.errors['File Format'] = 'File not formatted'
 
-		return payload
+        for var in variables:
+            if var not in raw_vars:
+                raw_vars.append(var)
+            case = var[3:-2]
+            case = tuple(case.split(', '))
+            if case not in payload:
+                payload.append(case)
 
-	def inputs(self, output=True):
-		"""
+        if raw:
+            return raw_vars
+        else:
+            return payload
+
+    def inputs(self, output_file=True):
+        """
 		Takes in a file pre-formatted with {$ variable, required, default_value }
 		symbols. Returns a payload dict() using {'variable': response} formatting.
-		dict(added_functions) includes {SYSTEM_CALL : function} to aquire default value.
-		:param added_functions: dict(optional)
+        Output declares whether or not the function should call the outputs function
+        after gathering inputs.
+
+		:param output_file: bool()
 		:return payload: dict()
 		"""
 
-		def send_payload(payload):
-			if not self.errors:
-				return payload
-			else:
-				return self.errors
+        SYSTEM_CALLS = {
+            '$USER': os.getlogin(),
+            '$PWD': os.getcwd(),
+            '$ENV_DIR': default_env_dir(os.getcwd()),
+            '$SOCK_DIR': os.getcwd() + '/' + os.getcwd().split('/')[-1] + '.sock',
+            '$PROJECT_NAME': os.getcwd().split   ('/')[-1],
+        }
 
-		def read_from_file(added_functions={}):
-			payload = {}
+        def read_from_file(added_functions={}):  # TODO docstring
+            payload = {}
 
-			SYSTEM_CALLS = {
-				'$USER': os.getlogin(),
-				'$PWD': os.getcwd(),
-				'$ENV_DIR': default_env_dir(os.getcwd()),
-				'$SOCK_DIR': os.getcwd() + '/' + os.getcwd().split('/')[-1] + '.sock',
-				'$PROJECT_NAME': os.getcwd().split('/')[-1],
-			}
+            if not os.path.isfile(self.file_path):
+                self.errors['No File'] = 'File is not at location'
 
-			if len(added_functions.keys()) >= 1:
-				SYSTEM_CALLS.update(added_functions)
+            if len(added_functions.keys()) >= 1:
+                SYSTEM_CALLS.update(added_functions)
 
-			for case in self.pull_vars():
-				prompt, required, default = case[0], case[1], case[2]
-				if default.startswith('$'):
-					default = SYSTEM_CALLS[default]
+            for case in self.pull_vars():
+                prompt, required, default = case[0], case[1], case[2]
+                if default.startswith('$'):
+                    default = SYSTEM_CALLS[default]
 
-				if prompt not in payload.keys():
-					if required != 'True':
-						user_input = input(f'{prompt}\n[{default}] : ')
-						if user_input == '':
-							user_input = default
-					else:
-						user_input = input(f'{prompt} : ')
-						while user_input == '':
-							user_input = input(f'{prompt} : ')
+                if prompt not in payload.keys():
+                    if required != 'True':
+                        user_input = input(f'{prompt}\n[{default}] : ')
+                        if user_input == '':
+                            user_input = default
+                    else:
+                        user_input = input(f'{prompt} : ')
+                        while user_input == '':
+                            user_input = input(f'{prompt} : ')
 
-					payload[prompt] = user_input
+                    payload[prompt] = user_input
 
-			return payload
+            return payload
 
-		if len(self.errors.keys()) == 0:
-			reading = read_from_file()
-			if reading and output:
-				self.outputs(reading, self.file_path)
+        if len(self.errors.keys()) == 0:
+            reading = read_from_file()
+            if reading and output_file:
+                self.outputs(reading)
+            elif reading and not output_file:
+                return reading
+            else:
+                self.errors['File Read'] = 'Error while reading inputs from file'
+                self.read_err()
 
-
-	def outputs(self, data, file):
-		"""
+    def outputs(self, data):  # TODO docstring
+        """
 		Takes data as dict() and writes to file based upon
-		which template is passed.
+		which template is passed as self.file_path.
+
 		:param data: dict()
 		:return:
 		"""
 
-		filename = file.split('/')[-1]
-		write_file = f'./new_{filename}'
-		print(data)
-		print(write_file)
+        filename = self.file_path.split('/')[-1]
+        write_to = f'./new_{filename}'
+        collected = [x for x in data.values()]
+        to_replace = self.pull_vars(raw=True)
+        ready = dict(zip(to_replace, collected))
+
+        if os.path.isfile(write_to):
+            self.errors['File Exists'] = f'File {filename} already found at {write_to}'
+            complete = False
+        else:
+            with open(write_to, 'w+') as f:
+                file = self.file
+                for k in ready.keys():
+                    file = file.replace(k, ready[k])  # Replaces all symbols in file with values
+
+                f.write(file)
+
+            if os.path.isfile(write_to):
+                print(f'File - {write_to} created')
+                complete = True
+            else:
+                self.errors['File Write'] = 'Error while writing file'
+                self.read_err()
+                complete = False
+
+        return complete
